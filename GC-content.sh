d6202682d55d7f52cc9170be9659453869297cde
wangshuaibin1015 <765278332@qq.com>
2018-07-01 12:02:10 +0800

    Update GC-content.sh

--- GC-content.sh
+++ GC-content.sh
@@ -1,1 +1,3 @@
-
+cat $1 | grep -v \> | grep -E -o "G|C" | wc -l > /tmp/GC_content
+cat $1 | grep -v \> | grep -E -o "G|C|A|T" | wc -l >> /tmp/GC_content
+cat /tmp/GC_content | awk '{getline total; print $0/total}'
